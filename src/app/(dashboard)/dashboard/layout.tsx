d33b611e5cfdb9767365cc11394316da6f9012e0
almostJohn <garcia.johngale@gmail.com>
2025-05-31 11:30:48 +0800

    fix: site config for better metadata SEO
    * move manrope font to its own file for better extensibility

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -3,21 +3,26 @@ import type { Metadata } from "next";
 import { redirect } from "next/navigation";
 import { getSession, type AuthPayload } from "@/lib/auth";
 import { Sidebar } from "../_components/sidebar";
+import { siteConfig } from "@/config/site";
 
 export const metadata: Metadata = {
 	title: "Dashboard",
+	description: siteConfig.description,
 	appleWebApp: {
-		title: "Syntra",
+		title: siteConfig.name,
 	},
-	applicationName: "Syntra",
+	applicationName: siteConfig.name,
 	openGraph: {
-		siteName: "Syntra",
+		siteName: siteConfig.name,
 		type: "website",
-		title: "Syntra",
+		title: siteConfig.title,
+		description: siteConfig.description,
 	},
 	twitter: {
 		card: "summary_large_image",
 		creator: "@almostJohn",
+		title: siteConfig.title,
+		description: siteConfig.description,
 	},
 	creator: "@almostJohn",
 };
--- src/config/site.ts
+++ src/config/site.ts
@@ -1,11 +1,11 @@
 export const siteConfig = {
 	name: "Syntra",
-	description:
-		"Syntra is a fast, minimalist note-taking app designed for clarity and productivity. Capture thoughts, organize ideas, and stay focused with a sleek, user-friendly interface — all in one place.",
 	title: {
 		default: "Syntra",
 		template: "Syntra — %s",
 	},
+	description:
+		"Syntra is a fast, minimalist note-taking app designed for clarity and productivity. Capture thoughts, organize ideas, and stay focused with a sleek, user-friendly interface — all in one place.",
 	creator: "@almostJohn",
 };
 
