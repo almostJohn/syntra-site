0fadb8ae53bcd032814b34d9f216478750b03935
almostJohn <garcia.johngale@gmail.com>
2025-06-22 11:35:51 +0800

    feat: add profile page + its dynamic UI
    * feat(ui): add activity heatmap as well for profile page
    
    * fix: update note server action not creating any notifications for any updated notes

--- src/app/(dashboard)/dashboard/(profile)/profile/page.tsx
+++ src/app/(dashboard)/dashboard/(profile)/profile/page.tsx
@@ -1,7 +1,21 @@
-export default function ProfilePage() {
+import { redirect } from "next/navigation";
+import { getCurrentUser } from "@/lib/auth";
+import { UserProfile } from "@/components/dashboard/profile/user-profile";
+import { Activity } from "@/components/dashboard/profile/activity";
+
+export default async function ProfilePage() {
+	const currentUser = await getCurrentUser();
+
+	if (!currentUser) {
+		redirect("/login");
+	}
+
 	return (
-		<div className="p-8 min-h-screen bg-muted flex flex-col space-y-6">
-			<div>Profile Page</div>
+		<div className="p-8 bg-muted min-h-screen">
+			<div className="mx-auto max-w-3xl flex flex-col space-y-6">
+				<Activity userId={currentUser.id} />
+				<UserProfile user={currentUser} />
+			</div>
 		</div>
 	);
 }
