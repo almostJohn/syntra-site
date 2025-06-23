8574223ba2303db5bfc54d2c245cb1071cabb542
almostJohn <garcia.johngale@gmail.com>
2025-06-23 23:58:56 +0800

    fix it by removing metadata title

--- src/app/(verification)/verification/result/page.tsx
+++ src/app/(verification)/verification/result/page.tsx
@@ -2,10 +2,6 @@
 
 import { Result } from "@/components/verification/result";
 
-export const metadata = {
-	title: "Verification Result",
-};
-
 export default function VerificationResultPage() {
 	return (
 		<div className="mx-auto max-w-2xl px-6 md:px-0 h-screen w-screen flex items-center justify-center">
