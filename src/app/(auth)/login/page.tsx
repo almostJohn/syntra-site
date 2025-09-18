498a98b51f3f31df5f7c76251ea17c45f7ba63e0
almostJohn <garcia.johngale@gmail.com>
2025-09-18 23:43:29 +0800

    add necessary files

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -7,15 +7,26 @@ export const metadata = {
 
 export default function LoginPage() {
 	return (
-		<MainLayout>
-			<div className="flex h-screen w-screen flex-col items-center justify-center px-8 sm:px-6">
-				<div className="flex w-full max-w-md flex-col justify-center gap-6">
-					<div className="flex flex-col gap-2 text-center">
-						<h3 className="text-2xl font-semibold tracking-tight">
-							Welcome back
+		<MainLayout className="flex">
+			<div className="border-border hidden w-1/3 overflow-hidden border-r-2 md:block">
+				<div className="flex min-h-screen flex-col items-end justify-center pr-10">
+					<h3 className="text-5xl font-bold tracking-tighter text-blue-600">
+						Sign In
+					</h3>
+					<p className="text-muted-foreground mt-2 text-right text-base">
+						Enter your username and password.
+					</p>
+				</div>
+			</div>
+			<div className="flex w-full items-center justify-start px-6 md:w-2/3 md:pl-10">
+				<div className="w-full max-w-lg">
+					{/* Mobile Header */}
+					<div className="mx-auto mb-8 flex flex-col justify-center text-center md:hidden">
+						<h3 className="text-3xl font-bold tracking-tight text-blue-600">
+							Sign In
 						</h3>
-						<p className="text-muted-foreground text-sm">
-							Log in to continue to your account.
+						<p className="text-muted-foreground mt-2 text-base">
+							Enter your username and password.
 						</p>
 					</div>
 					<LoginForm />
--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -7,14 +7,14 @@ export const metadata = {
 
 export default function RegisterPage() {
 	return (
-		<MainLayout>
-			<div className="flex h-screen w-screen items-center justify-center px-8 sm:px-6">
-				<div className="flex w-full max-w-md flex-col justify-center gap-6">
-					<div className="flex flex-col gap-2 text-center">
-						<h3 className="text-2xl font-semibold tracking-tight">
-							Create an account
+		<MainLayout className="flex flex-col items-center justify-center">
+			<div className="w-full max-w-md">
+				<div className="bg-card flex flex-col rounded-xl border p-6 shadow-2xl shadow-blue-600/30">
+					<div className="mb-8 flex flex-col justify-center text-center">
+						<h3 className="text-3xl font-bold tracking-tight text-blue-600">
+							Create Account
 						</h3>
-						<p className="text-muted-foreground text-sm">
+						<p className="text-muted-foreground mt-2 text-base">
 							Enter your information to get started.
 						</p>
 					</div>
